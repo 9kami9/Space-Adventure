13effc0e9c865c164cabf43e2c57335f8ff66cae
9kami9 <109879942+9kami9@users.noreply.github.com>
2022-07-30 18:14:28 +0300

    Quit application

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -18,7 +18,8 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (period == 0) { return; }
+        if (period <= Mathf.Epsilon) { return; }
+
         float cycles = Time.time / period; // continually growing over time
         const float tau = Mathf.PI * 2;
         float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
